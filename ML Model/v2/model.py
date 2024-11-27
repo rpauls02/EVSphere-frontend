3fe24a53ff4f1572e03feb5b46f769a3d1750c01
Anson Lam <anson.lam@city.ac.uk>
2024-11-27 21:31:54 +0000

    v2 updated

--- ML Model/v2/model.py
+++ ML Model/v2/model.py
@@ -81,7 +81,7 @@ y = dataset['Charging Duration (hours)']
 # split into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# train linear regresion model
+# train linear / support vector regression model
 #model = LinearRegression()
 model = SVR(kernel='rbf', C=10, epsilon=0.001)
 model.fit(X_train, y_train)
